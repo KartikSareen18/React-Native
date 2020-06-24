b0b2f48e8bc11826309f67ebba4c69b037116606
KartikSareen18 <kartiksareen18@gmail.com>
2020-06-24 11:30:32 +0530

    Secure Storage

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -5,6 +5,7 @@ import Dishdetail from './DishdetailComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
+import Login from './LoginComponent';
 import Favorites from './FavoriteComponent';
 import {View,Platform,Image,StyleSheet,ScrollView,Text} from 'react-native';
 import { createStackNavigator ,createDrawerNavigator,DrawerItems,SafeAreaView} from 'react-navigation';
@@ -139,6 +140,23 @@ const FavoritesNavigator = createStackNavigator({
     })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login:{screen:Login}
+},{
+    navigationOptions:({navigation})=>( {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        },
+        headerLeft: <Icon name='menu' size={24} 
+        color='white'
+        onPress={() => navigation.toggleDrawer()}/>
+    })
+});
+
 const CustomDrawerContentComponent = (props) =>{
     return(
         <ScrollView>
@@ -160,6 +178,16 @@ const CustomDrawerContentComponent = (props) =>{
 }
 
 const MainNavigator = createDrawerNavigator({
+    Login: {
+        screen: LoginNavigator,
+        navigationOptions:{
+            title: 'Login',
+            drawerLabel:'Login',
+            drawerIcon:({ tintColor })=>(
+                <Icon name='sign-in' type='font-awesome' size={24} color={tintColor}/>
+            ) 
+        }
+    },
     Home: {
         screen: HomeNavigator,
         navigationOptions:{
@@ -222,6 +250,7 @@ const MainNavigator = createDrawerNavigator({
     }
 
 },{
+    initialRouteName:'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent:CustomDrawerContentComponent 
 });
