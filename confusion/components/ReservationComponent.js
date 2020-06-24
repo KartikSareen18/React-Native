69d5c7f4169f09e1911e954762a762dbd563a0cf
KartikSareen18 <kartiksareen18@gmail.com>
2020-06-24 12:07:19 +0530

    Local Notifications

--- confusion/components/ReservationComponent.js
+++ confusion/components/ReservationComponent.js
@@ -3,6 +3,8 @@ import {Text,View,ScrollView,StyleSheet,Picker,Switch,Button, Modal,Alert} from 
 import {Card} from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component{
 
@@ -39,7 +41,10 @@ class Reservation extends Component{
                 },
                 {
                     text:'OK',
-                    onPress:()=>this.resetForm()
+                    onPress:()=>{
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
                 }
             ],
             {cancelable:false}
@@ -53,6 +58,32 @@ class Reservation extends Component{
             guest:1,
             smoking:false,
             date:''
+        });
+    }
+
+    async obtainNotificationPermission(){
+        let permission= await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if(permission.status!=='granted'){
+            permission= await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if(permission.status!=='granted')
+                Alert.alert('Permission not granted to show notifications');
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
         });
     }
 
